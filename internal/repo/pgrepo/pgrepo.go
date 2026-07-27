af5c5e2a58362a98b890978c2cd47be3e7f5c17a
Aleksandr Panfilov <aaa2ppp@gmail.com>
2026-07-27 16:51:15 +0300

    feat(repo): implement PostgreSQL repository with CRUD, soft delete and optimistic locking

--- internal/repo/pgrepo/pgrepo.go
+++ internal/repo/pgrepo/pgrepo.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"strings"
+	"subscriptions/internal/model"
 	"subscriptions/internal/repo"
+	"subscriptions/internal/service"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type PGRepo struct {
-	db *pgxpool.Pool
-}
+type PGRepo pgxpool.Pool
+
+func (repo *PGRepo) db() *pgxpool.Pool { return (*pgxpool.Pool)(repo) }
 
 func Open(ctx context.Context, cfg repo.Config) (*PGRepo, error) {
 	host, port, err := net.SplitHostPort(cfg.Addr)
@@ -27,9 +31,152 @@ func Open(ctx context.Context, cfg repo.Config) (*PGRepo, error) {
 		return nil, err
 	}
 
-	return &PGRepo{db: pool}, nil
+	return (*PGRepo)(pool), nil
 }
 
 func (repo *PGRepo) Close() {
-	repo.db.Close()
+	repo.db().Close()
 }
+
+func (r *PGRepo) Create(ctx context.Context, req model.Subscription) (model.Subscription, error) {
+	const sql = `INSERT INTO subscription (service_name, price, user_id, start_date, end_date) 
+	VALUES ($1, $2, $3, $4, $5) RETURNING id, created, updated;`
+
+	row := r.db().QueryRow(ctx, sql, req.ServiceName, req.Price, req.UserID, req.StartDate, req.EndDate)
+	if err := row.Scan(
+		&req.ID,
+		&req.Created,
+		&req.Updated,
+	); err != nil {
+		return model.Subscription{}, err
+	}
+
+	return req, nil
+}
+
+func (r *PGRepo) Get(ctx context.Context, id int64) (model.Subscription, error) {
+	const sql = `SELECT id, service_name, price, user_id, start_date, end_date, created, updated
+	FROM subscription WHERE id=$1 AND deleted IS NULL;`
+
+	var resp model.Subscription
+
+	row := r.db().QueryRow(ctx, sql, id)
+	if err := row.Scan(
+		&resp.ID,
+		&resp.ServiceName,
+		&resp.Price,
+		&resp.UserID,
+		&resp.StartDate,
+		&resp.EndDate,
+		&resp.Created,
+		&resp.Updated,
+	); err != nil {
+		if err == pgx.ErrNoRows {
+			err = model.ErrNotFound
+		}
+		return model.Subscription{}, err
+	}
+
+	return resp, nil
+}
+
+func (r *PGRepo) List(ctx context.Context, req model.ListSubscriptionsRequest) ([]model.Subscription, error) {
+	const sql = `SELECT id, service_name, price, user_id, start_date, end_date, created, updated
+	FROM subscription 
+	WHERE id > $1 AND start_date <= $3 AND $2 <= end_date AND deleted IS NULL %s
+	ORDER BY id %s;`
+
+	values := []any{req.AfterID, req.FromDate, req.ToDate}
+	n := 4
+	var filters []string
+	var limit string
+
+	if req.UserID.Valid {
+		filters = append(filters, fmt.Sprintf(" AND user_id = $%d", n))
+		values = append(values, req.UserID.V)
+		n++
+	}
+
+	if req.ServiceName.Valid {
+		filters = append(filters, fmt.Sprintf(" AND service_name = $%d", n))
+		values = append(values, req.ServiceName.V)
+		n++
+	}
+
+	if req.Limit > 0 {
+		limit = fmt.Sprintf("LIMIT $%d", n)
+		values = append(values, req.Limit)
+		n++
+	}
+
+	rows, err := r.db().Query(ctx, fmt.Sprintf(sql, strings.Join(filters, ""), limit), values...)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := []model.Subscription{}
+	for rows.Next() {
+		resp = append(resp, model.Subscription{})
+		i := len(resp) - 1
+		if err := rows.Scan(
+			&resp[i].ID,
+			&resp[i].ServiceName,
+			&resp[i].Price,
+			&resp[i].UserID,
+			&resp[i].StartDate,
+			&resp[i].EndDate,
+			&resp[i].Created,
+			&resp[i].Updated,
+		); err != nil {
+			return nil, err
+		}
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (r *PGRepo) Update(ctx context.Context, req model.Subscription) (model.Subscription, error) {
+	var zero model.Subscription
+
+	const sql = `UPDATE subscription 
+	SET service_name=$2, price=$3, user_id=$4, start_date=$5, end_date=$6, updated=NOW()
+	WHERE id = $1 AND updated = $7 AND deleted IS NULL
+	RETURNING updated;`
+
+	row := r.db().QueryRow(ctx, sql,
+		req.ID,
+		req.ServiceName,
+		req.Price,
+		req.UserID,
+		req.StartDate,
+		req.EndDate,
+		req.Updated,
+	)
+
+	err := row.Scan(&req.Updated)
+
+	if err != nil && err != pgx.ErrNoRows {
+		return zero, err
+	}
+
+	if err == pgx.ErrNoRows {
+		old, err := r.Get(ctx, req.ID)
+		if err != nil {
+			return zero, err
+		}
+		return old, fmt.Errorf("%w: optimistic locking conflict (2)", model.ErrConflict)
+	}
+
+	return req, nil
+}
+
+func (r *PGRepo) Delete(ctx context.Context, id int64) error {
+	const sql = `UPDATE subscription SET deleted = NOW() WHERE id = $1 AND deleted IS NULL;`
+	_, err := r.db().Exec(ctx, sql, id)
+	return err
+}
+
+var _ service.Repository = &PGRepo{}
